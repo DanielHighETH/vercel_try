faa789c30b0e96c62b6a224af843e9ab9d48397c
DanielHighETH <danielhigheth@gmail.com>
2022-10-29 01:24:23 +0200

    test

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const port = 3000;
 
 app.get("/", (req, res) => {
     res.json("Hello")
+    res.send("cauaau")
     //res.sendFile(__dirname + "/public/index.html")
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile("/api/about.html")
+    res.send("cauaau")
+    //res.sendFile("/api/about.html")
 });
 
 app.get("/index", (req, res) => {
