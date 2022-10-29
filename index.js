0f56a3026fbc5a87d9bb93215b01ba1efc02c4bb
DanielHighETH <danielhigheth@gmail.com>
2022-10-29 13:10:35 +0200

    ed

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ app.listen(process.env.PORT || 3000);
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send("caucau")
-    //res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-  })
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/dva.html")
+});
 
-  app.get('/borec', (req, res) => {
-    res.sendFile('borec.html', {root: path.join(__dirname, 'public')});
-  })
+app.get("/borec", (req, res) => {
+  res.sendFile(__dirname + "/public/borec.html")
+});
+
+  app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+  });
+  
 
   module.exports = app
